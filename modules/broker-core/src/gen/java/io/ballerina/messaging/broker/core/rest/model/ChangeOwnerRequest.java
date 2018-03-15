7b3ce795a39a5f427ea7bec7d07683ead7c830a6
Asitha Nanayakkara <daun07@gmail.com>
2018-03-15 05:45:44 +0530

    Update grant permission swagger definition

--- modules/broker-core/src/gen/java/io/ballerina/messaging/broker/core/rest/model/ChangeOwnerRequest.java
+++ modules/broker-core/src/gen/java/io/ballerina/messaging/broker/core/rest/model/ChangeOwnerRequest.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
 package io.ballerina.messaging.broker.core.rest.model;
 
 import javax.validation.constraints.*;
@@ -22,8 +41,9 @@ public class ChangeOwnerRequest   {
   }
 
   
-  @ApiModelProperty(value = "Username of the new owner")
+  @ApiModelProperty(required = true, value = "Username of the new owner")
   @JsonProperty("owner")
+  @NotNull
   public String getOwner() {
     return owner;
   }
@@ -33,7 +53,7 @@ public class ChangeOwnerRequest   {
 
 
   @Override
-  public boolean equals(java.lang.Object o) {
+  public boolean equals(Object o) {
     if (this == o) {
       return true;
     }
@@ -63,7 +83,7 @@ public class ChangeOwnerRequest   {
    * Convert the given object to string with each line indented by 4 spaces
    * (except the first line).
    */
-  private String toIndentedString(java.lang.Object o) {
+  private String toIndentedString(Object o) {
     if (o == null) {
       return "null";
     }
