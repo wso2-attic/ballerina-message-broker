65a60e8afb8b30409a3850110d06b0cf3b98fa31
DMadhuranga <danushka.15@cse.mrt.ac.lk>
2018-12-03 10:02:14 +0530

    Changing message format

--- modules/broker-cli-client/src/main/java/io/ballerina/messaging/broker/client/http/HttpClient.java
+++ modules/broker-cli-client/src/main/java/io/ballerina/messaging/broker/client/http/HttpClient.java
@@ -145,9 +145,8 @@ public class HttpClient {
             return new HttpResponse(responseCode, response.toString());
         } catch (ConnectException e) {
             BrokerClientException exception = new BrokerClientException();
-            exception.addMessage("Error calling broker https services.");
-            exception.addMessage("Please check whether the broker is running or the connectivity to the broker is not"
-                                 + " blocked through a firewall.");
+            exception.addMessage("CLI tool cannot access " + url + ". \nPlease check whether the broker is running or"
+                                 + " the connectivity to the broker is not blocked through a firewall.");
             exception.addMessage(e.getMessage());
             throw exception;
         } catch (IOException e) {
