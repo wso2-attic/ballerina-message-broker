b2ae35a6416734a7e11bda4e2dc68d45c043e488
KasunPerera1995 <kasunperera1995@gmail.com>
2018-12-29 07:07:28 +0530

    Remove body of event notifications

--- modules/broker-core/src/main/java/io/ballerina/messaging/broker/core/eventpublisher/DefaultExchangePublisher.java
+++ modules/broker-core/src/main/java/io/ballerina/messaging/broker/core/eventpublisher/DefaultExchangePublisher.java
@@ -21,15 +21,10 @@ package io.ballerina.messaging.broker.core.eventpublisher;
 import io.ballerina.messaging.broker.common.data.types.FieldValue;
 import io.ballerina.messaging.broker.core.Broker;
 import io.ballerina.messaging.broker.core.BrokerException;
-import io.ballerina.messaging.broker.core.ContentChunk;
 import io.ballerina.messaging.broker.core.Message;
 import io.ballerina.messaging.broker.core.Metadata;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 /**
@@ -49,28 +44,16 @@ public class DefaultExchangePublisher implements ExchangePublisher {
         //Name of the Exchange where notifications are published.
         String exchangeName = "x-event";
 
-        String data = "Event Message";
-        Metadata metadata = new Metadata(id, exchangeName, data.length());
+        Metadata metadata = new Metadata(id, exchangeName, 0);
         metadata.setHeaders(properties);
         //Transfer to metadata
         metadata.addProperty("propertyFlags", FieldValue.parseLongInt(8192));
         Message notificationMessage = new Message(Broker.getNextMessageId(), metadata);
-
-        //Creating the body of the message
-        notificationMessage.addChunk(getChunk(data));
 
         try {
             broker.publish(notificationMessage);
         } catch (BrokerException e) {
             LOGGER.warn("Exception while publishing event notification message {}", notificationMessage, e);
         }
-    }
-
-    private ContentChunk getChunk(String data) {
-        byte[] dataBytes = data.getBytes(StandardCharsets.UTF_8);
-        int chunkSize = dataBytes.length;
-        int offset = 0;
-        ByteBuf buffer = Unpooled.wrappedBuffer(dataBytes, offset, Math.min(chunkSize, dataBytes.length - offset));
-        return new ContentChunk(offset, buffer);
     }
 }
